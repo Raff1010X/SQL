5be5a4ac900f6afabf8d7e7d1be509733769c2b8
Raff1010X <86784368+Raff1010X@users.noreply.github.com>
2022-05-09 11:55:18 +0200

    update

--- worldcup/queries.sh
+++ worldcup/queries.sh
@@ -26,16 +26,16 @@ echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo  $($PSQL "SELECT COUNT(winner_goals) FROM games WHERE winner_goals > 2")
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo
+echo  $($PSQL "SELECT name FROM games FULL JOIN teams ON games.winner_id = teams.team_id WHERE year='2018' AND round='Final'")
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo  "$($PSQL "SELECT name FROM games INNER JOIN teams ON games.winner_id = teams.team_id OR games.opponent_id = teams.team_id WHERE year='2014' AND round='Eighth-Final' ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo  "$($PSQL "SELECT DISTINCT(name) FROM games INNER JOIN teams ON games.winner_id = teams.team_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo  "$($PSQL "SELECT year, name FROM games INNER JOIN teams ON games.winner_id = teams.team_id WHERE round='Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo  "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%' ORDER BY name")"
